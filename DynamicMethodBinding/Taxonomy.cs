be17d27068d2de5685bf0fa3cc124455f27198e7
Rafael Romão <rafaelromao@gmail.com>
2013-04-28 21:09:57 -0300

    Complete Source Code

---
+++ DynamicMethodBinding/Taxonomy.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DynamicMethodBinding
+{
+    public interface IAnimal
+    {
+        string GenusName { get; }
+    }
+
+    public class Lion : IAnimal 
+    {
+        public string GenusName { get { return "Panthera"; } }
+    }
+
+    public class Zebra : IAnimal 
+    {
+        public string GenusName { get { return "Equus"; } }
+    }
+
+    public class Elephant : IAnimal 
+    {
+        public string GenusName { get { return "Loxodonta"; } }
+    }
+
+    public class Taxonomy
+    {
+        #region This is the only public method!
+
+        public string GenusFor(IAnimal animal)  
+        {
+            // Which version of GenusFor will be called? It depends on the concrete type of 'animal'.
+            return ((dynamic)this).GenusFor((dynamic)animal);
+        }
+
+        #endregion
+
+        #region These are private methods!
+
+        private string GenusFor(Lion lion)
+        {
+            return "Lion genus is " + lion.GenusName;
+        }
+
+        private string GenusFor(Zebra zebra)
+        {
+            return "Zebra genus is " + zebra.GenusName;
+        }
+
+        private string GenusFor(Elephant elephant)
+        {
+            return "Elephant genus is " + elephant.GenusName;
+        }
+
+        #endregion
+    }
+}
---
+++ DynamicMethodBinding/TaxonomyTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace DynamicMethodBinding
+{
+    [TestFixture]
+    public class TaxonomyTests
+    {
+        [Test]
+        public void TestGenusForDynamicBinding()
+        {
+            var taxonomy = new Taxonomy(); 
+            
+            var lion = new Lion();
+            Assert.AreEqual("Lion genus is Panthera", taxonomy.GenusFor(lion));
+
+            var zebra = new Zebra();
+            Assert.AreEqual("Zebra genus is Equus", taxonomy.GenusFor(zebra));
+
+            var elephant = new Elephant();
+            Assert.AreEqual("Elephant genus is Loxodonta", taxonomy.GenusFor(elephant));
+
+            // The public Taxonomy.GenusFor method delegates to the best version of the private Taxonomy.GenusFor methods
+            // the responsibility to tell the genus for the given IAnimal.
+            // This shows how dynamic method binding works.
+        }
+    }
+}
